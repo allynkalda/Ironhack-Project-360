0506671585c48b627015579f7bd9fb16c3187329
Allyn Alda <Teddy@Om.home>
2019-05-05 13:25:50 +0200

    added routes from users to survey

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,24 @@
 const express = require('express')
 const router = express.Router()
 const Users = require('./../models/usersSchema')
+const Survey = require('./../models/surveySchema')
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' })
 })
 
-/* GET Directory page. */
+/* GET Users page. */
 router.get('/users', function (req, res, next) {
-  Users.find({ })
+  Users.find({})
     .then((users) => res.render('users', { users }))
     .catch((err) => console.log(err))
+})
+
+/* GET Survey page. */
+router.get('/survey/:id', function (req, res, next) {
+  const { id } = req.params
+  res.render('survey')
 })
 
 module.exports = router
