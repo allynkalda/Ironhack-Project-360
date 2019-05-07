8dd569483cd8d169d8b25c0f510216fdea26516a
Eric Capdevila <eric241@hotmail.es>
2019-05-07 09:12:12 +0200

    added the social resutls to the charts and they display correctly, proceeding to add the rest of the results

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,8 +1,4 @@
 
-// const express = require('express')
-// const router = express.Router()
-// const Surveys = require('../models/surveySchema')
-// const User = require('../models/usersSchema')
 
 const baseURL = 'http://localhost:3007'
 const canvas = document.querySelector('canvas')
@@ -10,24 +6,18 @@ const canvas = document.querySelector('canvas')
 window.addEventListener('load', () => {
   printSocialChart()
 })
-// get array of results by skill, for the charts
-
-// const printSocialChart = (surveyApi, surveyNum) => {
   
 function printSocialChart () {
-
   axios.get(`${baseURL}/results`)
   .then((response) => {
-   console.log(response)
-  })
-  const social = document.getElementById('socialChart').getContext('2d')
+    const social = document.getElementById('socialChart').getContext('2d')
     var socialChart = new Chart(social, {
     type: 'bar',
     data: {
         labels: [1, 2, 3, 4],
         datasets: [{
           label: "SOCIAL",
-          data: [13, 24, 35, 12],
+          data: response.data,
           backgroundColor: [
             'rgba(255, 99, 132, 0.2)',
             'rgba(54, 162, 235, 0.2)',
@@ -53,17 +43,6 @@ function printSocialChart () {
         }
       }
     })
-  }
-// surveyApi.get(`/results`)
-//   .then((response) => printSocialChart(response.data, 1))
-//   .catch((error) => console.log(error))
+  })
+}
 
-// render charts
-// router.get('/results', function (req, res, next) {
-  // res.render('results')
-  // })
-  
-  // create route
-  // get the data with postman
-  // normalize data
-  // res
