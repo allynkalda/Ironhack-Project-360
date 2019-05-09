129fd9a98cafc853fa5bbb02f19155116a8886c8
Eric Capdevila <eric241@hotmail.es>
2019-05-09 09:28:06 +0200

    added CORS middleware

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'hbs')
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Origin', 'https://project-360.herokuapp.com')
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
   next()
 })
