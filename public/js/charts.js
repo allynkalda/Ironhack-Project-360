e7480603b02082d909478fad5924d5e82d4e33cc
Eric Capdevila <eric241@hotmail.es>
2019-05-09 09:37:12 +0200

    change typo in baseUrl in chart.js

--- public/js/charts.js
+++ public/js/charts.js
@@ -1,6 +1,6 @@
 
 //const baseURL = 'http://localhost:3007'
-const baseURL = 'https://project-360.herokuapp.com/';
+const baseURL = 'https://project-360.herokuapp.com';
 const canvas = document.querySelector('canvas')
 
 
